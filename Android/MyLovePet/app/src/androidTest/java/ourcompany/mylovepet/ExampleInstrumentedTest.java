0ffda25c229b48f83b146a31c0e25760699d9409
jjonghun1004 <jjonghun1004@naver.com>
2017-04-18 14:53:28 +0900

    useAppContext 수정
    24번째 줄 주석 추가

--- Android/MyLovePet/app/src/androidTest/java/ourcompany/mylovepet/ExampleInstrumentedTest.java
+++ Android/MyLovePet/app/src/androidTest/java/ourcompany/mylovepet/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("ourcompany.mylovepet", appContext.getPackageName());
+        assertEquals("ourcompany.mylovepet", appContext.getPackageName());  // Test
     }
 }
