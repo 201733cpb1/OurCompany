916c72f39956c678584bd14a40af9fdcc5f2e753
Daemin <eoals0704@naver.com>
2017-05-30 17:22:52 +0900

    펫시터 글보기
    펫시터 글 보기

---
+++ Android/MyLovePet/app/src/main/java/ourcompany/mylovepet/petsitter/PetSitterViewActivity.java
@@ -0,0 +1,17 @@
+package ourcompany.mylovepet.petsitter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ourcompany.mylovepet.R;
+
+/**
+ * Created by KDM on 2017-05-16.
+ */
+
+public class PetSitterViewActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_petsitter_view);
+    }
+}
