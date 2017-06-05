612c837ee20e152277337d6810a6ebd7ad7f3f1d
Daemin <eoals0704@naver.com>
2017-06-05 15:56:07 +0900

    산책 거리 추가
    산책 거리 추가

--- Android/MyLovePet/app/src/main/java/ourcompany/mylovepet/main/PetWalkActivity.java
+++ Android/MyLovePet/app/src/main/java/ourcompany/mylovepet/main/PetWalkActivity.java
@@ -1,6 +1,13 @@
 package ourcompany.mylovepet.main;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -11,13 +18,20 @@ import java.util.Date;
 
 import ourcompany.mylovepet.R;
 
+import static android.location.LocationManager.GPS_PROVIDER;
+import static android.location.LocationManager.NETWORK_PROVIDER;
+
 /**
  * Created by KDM on 2017-05-16.
  */
 
 public class PetWalkActivity extends AppCompatActivity {
-    TextView start,end;
+    TextView start,end,distance;
     Button walkstart,walkend,totalend;
+    double total_distance;
+    LocationManager locationManager;
+    LocationListener locationListener;
+    
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet_walk);
@@ -27,6 +41,10 @@ public class PetWalkActivity extends AppCompatActivity {
         walkstart = (Button) findViewById(R.id.walkstart);
         walkend = (Button) findViewById(R.id.walkend);
         totalend = (Button) findViewById(R.id.end);
+        totalend = (Button) findViewById(R.id.end);
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
 
         walkstart.setOnClickListener(new Button.OnClickListener(){
             @Override
@@ -41,6 +59,48 @@ public class PetWalkActivity extends AppCompatActivity {
                 start.setText(formatDate);
                 walkstart.setVisibility(View.GONE);
                 walkend.setVisibility(View.VISIBLE);
+
+                locationListener = new LocationListener() {
+                    public void onLocationChanged(Location location) {
+                        Location lo1 = new Location("A"); // 현재 위치
+
+                        if(lo1==null){
+                            lo1 = location;
+                        } else{
+                            if(lo1 !=location){
+                                total_distance += lo1.distanceTo(location);
+                                lo1 = location;
+                            }
+                            lo1 = location;
+                        }
+                    }
+
+                    public void onStatusChanged(String provider, int status, Bundle extras) {
+                        //distance.setText("onStatusChanged");
+                    }
+
+                    public void onProviderEnabled(String provider) {
+                        //distance.setText("onProviderEnabled");
+                    }
+
+                    public void onProviderDisabled(String provider) {
+                        //distance.setText("onProviderDisabled");
+                    }
+                };
+                // Register the listener with the Location Manager to receive location updates
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                        && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                locationManager.requestLocationUpdates(NETWORK_PROVIDER, 1000, 10, locationListener);
+                locationManager.requestLocationUpdates(GPS_PROVIDER, 1000, 10, locationListener);
             }
         });
         walkend.setOnClickListener(new Button.OnClickListener(){
@@ -56,6 +116,9 @@ public class PetWalkActivity extends AppCompatActivity {
                 end.setText(formatDate);
                 totalend.setVisibility(View.VISIBLE);
                 walkend.setVisibility(View.GONE);
+
+                int dist = (int)total_distance;
+                distance.setText(dist/10000000+" M");
             }
         });
     }
